36a3cf7d50d885f95303f9de489ffa117cebd6f7
João P. Marques <joaop.marques@outlook.com>
2023-02-03 20:12:47 +0000

    feat(style): adds basic type and styles

---
+++ components/Heading.tsx
@@ -0,0 +1,12 @@
+export const Heading = () => {
+  return (
+    <header className="flex flex-col items-center justify-center max-w-3xl mt-40">
+      <h1 className="text-xl font-semibold text-silver-100 mb-6">
+        All apps in the world share the same DNA
+      </h1>
+      <p className="text-md text-silver-200">
+        Think about infrastructure, notifications, social auth. There’s over 70% overlap in functionality.
+      </p>
+    </header>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,29 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      orange: '#FFBB56',
+      green: '#45E3BE',
+      blue: '#2FBDEA',
+      midnight: '#1F2B48',
+      purple: '#8D65FF',
+      silver: {
+        100: '#FFFDF5',
+        200: '#C9C5D3',
+        300: '#ACAAC0',
+      }
+    },
+    fontFamily: {
+      'workSans': ['"Work Sans"', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+    },
+    fontSize: {
+      xs: ['18px', { lineHeight: '27px' }],
+      sm: ['26px', { lineHeight: '39px' }],
+      md: ['32px', { lineHeight: '48px' }],
+      lg: ['40px', { lineHeight: '52px' }],
+      xl: ['72px', { lineHeight: '94px' }],
+    },
     extend: {},
   },
   plugins: [],
