4c8845e14e93bbfa15122929f965c28fb98622fb
João P. Marques <joaop.marques@outlook.com>
2023-02-04 15:35:39 +0000

    style(prettier): formats codebase

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,10 +1,9 @@
 import { useRef, useMemo } from "react";
 
 /**
-* Function that returns the percentage scrolled by the user relative to the document full height
-* @returns {number} - Percentage scrolled (0-100)
-*/
-
+ * Function that returns the percentage scrolled by the user relative to the document full height
+ * @returns {number} - Percentage scrolled (0-100)
+ */
 
 export const getScrollPercentage = (): number => {
   const h = document.documentElement,
